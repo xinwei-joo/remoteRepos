3f0a9de97263a24145f96825bfb64b58e8396422
xinwei <wikiejoo@outlook.com>
2017-02-11 11:02:43 +0800

    master 修改 remote.java

--- remote.java
+++ remote.java
@@ -1,27 +1,7 @@
 public class Hello
 {
 
-
-	public int add(int a, int b){
-		return a + b;
-	}
-
-	public int sub(int a, int b){
-		return a - b;
-	}
-
-	public int div(int a, int b){
-
-		int result = 0;
-		if(b != 0)
-			result = a/b;
-		return result;
-	}
-
-	public int mul(int a, int b){
-		return a * b;
-	}
-
 	public static void main(String [] args){
+		System.out.println("master 修改 remote.java");
 	}
 }
