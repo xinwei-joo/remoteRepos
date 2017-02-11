3a6255ef3448722b4bcc9113af63e7d276f2f895
xinwei <wikiejoo@outlook.com>
2017-02-11 09:03:59 +0800

    添加 add, sub, div 函数

--- remote.java
+++ remote.java
@@ -1,5 +1,19 @@
 public class Hello
 {
+
+
+	public int add(int a, int b){
+		return a + b;
+	}
+
+	public int sub(int a, int b){
+		return a - b;
+	}
+
+	public int div(int a, int b){
+		return a / b;
+	}
+
 	public static void main(String [] args){
 	}
 }
